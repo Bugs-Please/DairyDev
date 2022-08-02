77f8f0b19ef08a27d8a62c566ca2cc70dc154ab6
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-08-02 19:53:46 +0530

    basic login/signp folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import { 
-  BrowserRouter as Router, 
-  Route,
-  Routes,
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
 } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Screens/LandingPage/LandingPage';
+import Login from './Screens/Login/Login';
+import Signup from './Screens/Signup/Signup';
 
 const App = () => {
     return (
         <Router>
             <Navbar />
             <Routes>
-                <Route exact path = '/' element = {LandingPage} />
+                <Route path='/' element={LandingPage} />
+                {/* <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/signup">
+                        <Signup />
+                    </Route> */}
             </Routes>
         </Router>
     )
---
+++ frontend/src/Screens/Signup/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Signup = () => {
+    return ( 
+        <div className="signup">
+            <h1>Signup page</h1>
+        </div>
+     );
+}
+ 
+export default Signup;
