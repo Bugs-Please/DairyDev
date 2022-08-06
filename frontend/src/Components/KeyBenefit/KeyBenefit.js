f9a5bfeb2b3ccdd6727c2c451f48103e592b322a
Amruta-26 <darakamruta@gmail.com>
2022-08-06 12:23:28 +0530

    Added KeyBenefit to components

---
+++ frontend/src/Components/KeyBenefit/KeyBenefit.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./KeyBenefit.css";
+const KeyBenefit = () => {
+  return (
+    <section classname="key-benefits">
+      <div className="container benefit-section-heading text-center">
+        <h2>Key Benefits</h2>
+      </div>
+
+      <div className="container key-benefits-content">
+        <div className="row">
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">
+              Organizing Small Farmers
+            </div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">Immutable</div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">
+              Build Trust among consumers
+            </div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">Tracing Milk</div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">
+              Prevent Counterfeiting of Dairy Products
+            </div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">Blockchain</div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">IOT</div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+
+          <div className="col-md benefit-cell p-4 m-3">
+            <div className="benefit-cell-heading text-center">RFID Tags</div>
+            <div className="benefit-cell-content text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              similique in autem esse voluptates doloribus et, velit ratione
+              quos iure.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default KeyBenefit;
--- frontend/src/Screens/LandingPage/LandingPage.js
+++ frontend/src/Screens/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./LandingPage.css";
+import KeyBenefit from "../../Components/KeyBenefit/KeyBenefit";
 const LandingPage = () => {
   return (
     <div>
@@ -8,98 +9,7 @@ const LandingPage = () => {
       {/* A component for process tab */}
 
       {/* benefits cards component */}
-      <section classname="key-benefits">
-        <div className="container benefit-section-heading text-center">
-          <h2>Key Benefits</h2>
-        </div>
-
-        <div className="container key-benefits-content">
-          <div className="row">
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">
-                Organizing Small Farmers
-              </div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">Immutable</div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">
-                Build Trust among consumers
-              </div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">
-                Tracing Milk
-              </div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">
-                Prevent Counterfeiting of Dairy Products
-              </div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">Blockchain</div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">IOT</div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-
-            <div className="col-md benefit-cell p-4 m-3">
-              <div className="benefit-cell-heading text-center">RFID Tags</div>
-              <div className="benefit-cell-content text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                similique in autem esse voluptates doloribus et, velit ratione
-                quos iure.
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
+      <KeyBenefit />
       {/* Advantages component */}
 
       {/* footer */}
