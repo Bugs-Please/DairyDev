148aa6f984c3dd0390984a9890bd8208d132d9b1
Amruta-26 <darakamruta@gmail.com>
2022-08-06 19:08:18 +0530

    Done basic KeyBenefit to components

--- frontend/src/Components/KeyBenefit/KeyBenefit.js
+++ frontend/src/Components/KeyBenefit/KeyBenefit.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./KeyBenefit.css";
+import lock from "../../Assets/images/Lock.png";
+import trace from "../../Assets/images/Trace.png";
+import digi from "../../Assets/images/Digi.png";
+
 const KeyBenefit = () => {
   return (
     <section classname="key-benefits">
@@ -89,6 +93,71 @@ const KeyBenefit = () => {
             </div>
           </div>
         </div>
+      </div>
+
+
+
+
+      <div className="container key-benefits-details my-5">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6">
+            <img
+              className=" w-50 mx-auto d-block"
+              src={lock}
+              alt="Lock to represent Immutablility"
+            />
+          </div>
+          <div className="col-md-6">
+            <h3 className="key-benefit-details-heading bold">Immutable</h3>
+            <p className="key-benefit-details-content">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestias eveniet dolore sint ratione, quidem voluptates saepe ex
+              blanditiis, hic quisquam dicta, animi doloribus. Autem similique
+              fuga eveniet dolorum vitae distinctio voluptatem quasi ipsa ipsum!
+            </p>
+          </div>
+        </div>
+
+
+        <div className="row justify-content-center align-items-center">
+        <div className="col-md-6 order-md-2">
+          <img
+              className="mx-auto d-block w-100"
+              src={trace}
+              alt="Tocality to represent traceability"
+            />
+          </div>
+          <div className="col-md-6 order-md-1">
+          <h3 className="key-benefit-details-heading bold">Tracing Milk</h3>
+            <p className="key-benefit-details-content">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestias eveniet dolore sint ratione, quidem voluptates saepe ex
+              blanditiis, hic quisquam dicta, animi doloribus. Autem similique
+              fuga eveniet dolorum vitae distinctio voluptatem quasi ipsa ipsum!
+            </p>
+          </div>
+        </div>
+
+
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6">
+            <img
+              className=" w-50 mx-auto d-block"
+              src={digi}
+              alt="Tab to represent digitize"
+            />
+          </div>
+          <div className="col-md-6">
+            <h3 className="key-benefit-details-heading bold">Digitize end to end process</h3>
+            <p className="key-benefit-details-content">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestias eveniet dolore sint ratione, quidem voluptates saepe ex
+              blanditiis, hic quisquam dicta, animi doloribus. Autem similique
+              fuga eveniet dolorum vitae distinctio voluptatem quasi ipsa ipsum!
+            </p>
+          </div>
+        </div>
+
       </div>
     </section>
   );
