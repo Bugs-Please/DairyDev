2cfd2f550962b69fd6182e29c9c566040cb84b75
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-08-22 00:55:42 +0530

    Login page completed

--- frontend/src/Screens/Login/Login.js
+++ frontend/src/Screens/Login/Login.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 import group from "./group.svg";
-import './Login.css';
+/* import './Login.css'; */
 import {
     FacebookLoginButton,
     GoogleLoginButton
 } from "react-social-login-buttons";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { Container, Row, Col } from "react-bootstrap";
 import { auth } from "../../firebase";
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const login = async(e) =>{
+    const login = async (e) => {
         e.preventDefault()
-        const res = await signInWithEmailAndPassword(auth,email,password);
+        const res = await signInWithEmailAndPassword(auth, email, password);
         console.log(res)
-        
+
     }
     return (
-        <div className="login">
+        /* <div className="login">
             <div className="container d-flex">
                 <div className="container1">
                     <img src={group} />
@@ -36,14 +37,14 @@ const Login = () => {
                     <form onSubmit={login}>
                         <div className="form-row">
                             <div className="form-group col-md-12">
-                                {/* <label for="inputEmail4">Email</label> */}
+                            
                                 <input type="email" className="form-control" id="inputEmail4" placeholder="Email" value={email} 
                                 onChange={e => setEmail(e.target.value)}/>
                             </div>
                             <br />
 
                             <div className="form-group col-md-12">
-                                {/* <label for="inputPassword4">Password</label> */}
+                            
                                 <input type="password" className="form-control" id="password1" placeholder="Password" value={password}
                                 onChange={e => setPassword(e.target.value)}/>                                              
                             </div>
@@ -63,7 +64,47 @@ const Login = () => {
                 </div>
 
             </div>
-        </div>
+        </div> */
+
+        <Row style={{ display: "flex", justifyContent: 'space-evenly', overflowX: 'hidden', overflowY: 'hidden' }}>
+
+            <Col className="d-sm-none d-md-block col-md-5">
+                <img src={group} style={{ height: "100vh" }} alt='milk can' />
+            </Col>
+            <Col className="col-12 col-md-7">
+                <div className="d-flex flex-column justify-content-center p-5 m-5">
+                    <h1 className="title text-center">Sign In</h1>
+                    
+                    
+                    <GoogleLoginButton />
+                    <br/>
+                    <FacebookLoginButton />
+                    <br/>
+                    
+
+                    <form onSubmit={login}>
+                            <div className="form-group col-md-12">
+                                <input type="email" className="form-control" id="inputEmail4" placeholder="Email" value={email}
+                                    onChange={e => setEmail(e.target.value)} />
+                            </div>
+
+                            <div className="form-group col-md-12">
+                                <input type="password" className="form-control" id="password1" placeholder="Password" value={password}
+                                    onChange={e => setPassword(e.target.value)} />
+                            </div>
+
+                            <div className="row mt-3">
+                                <button type="submit" className="btn rounded-3 mt-3 mb-3 btn-block btn-primary">Sign In</button>
+                            </div>
+
+                            <div className="signup-link">
+                                <a href="/signup" className="text-center text-decoration-none"><h5>Don't have an account? Register</h5></a>
+                            </div>
+
+                    </form>
+                </div>
+            </Col>
+        </Row>
     );
 }
 
