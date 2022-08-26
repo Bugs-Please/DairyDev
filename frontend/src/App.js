93003d457a583804acd9a9b71e24b266533db94f
Abhiavati20 <abhishekaa1112@gmail.com>
2022-08-26 13:49:07 +0530

    farmer details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,5 @@
-
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './Screens/Dashboard/Dashboard';
 import { AuthProvider } from './AuthProvider';
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -25,39 +20,46 @@ import ShippingBatchDetails from './Screens/Dashboard/ShippingBillDetails';
 import ProcessingBatchDetails from './Screens/Dashboard/ProcessingBillDetails';
 import QR from './Screens/QRtest/qr';
 import QRTracking from './Screens/QRTracking/QRTracking';
-
-
+import FarmerProfile from './Screens/Dashboard/FarmerProfile';
 const App = () => {
-    return (
-        <AuthProvider>
-        <Router>
-            <Routes>
-                <Route path = "/" element={<LandingPage />} />
-                <Route path = "/login" element={<Login />} />
-                <Route path = "/signup" element={<Signup />} />
-                <Route path = "/phone-signup" element = {<PhoneSignup />} />
-                <Route path = "/dashboard" element={<Dashboard/>} />
-                <Route path = "/tracking" element={<Tracking/>} />
-                <Route path = "/new-batch" element={<GenerateBatch/>} />
-                <Route path = "/farmers" element={<FarmerList/>} />
-                <Route path = "/transferbatch" element={<TransferBatch/>} />
-                <Route path = "/generateshippingbill" element={<GenerateShippingBill/>} />
-                <Route path = "/transfershippingbill" element={<TransferShippingBill/>} />
-                <Route path = "/generatelabreport" element={<GenerateLabReport/>} />
-                <Route path = "/transferlabreport" element={<TransferLabReport/>} />
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/phone-signup" element={<PhoneSignup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tracking" element={<Tracking />} />
+          <Route path="/new-batch" element={<GenerateBatch />} />
+          <Route path="/farmers" element={<FarmerList />} />
+          <Route path="/transferbatch" element={<TransferBatch />} />
+          <Route
+            path="/generateshippingbill"
+            element={<GenerateShippingBill />}
+          />
+          <Route
+            path="/transfershippingbill"
+            element={<TransferShippingBill />}
+          />
+          <Route path="/generatelabreport" element={<GenerateLabReport />} />
+          <Route path="/transferlabreport" element={<TransferLabReport />} />
+          <Route path="/farmer-profile" element={<FarmerProfile />} />
+          <Route path="/batch-details" element={<BatchDetails />} />
+          <Route path="/shipping-batch" element={<ShippingBatchDetails />} />
+          <Route
+            path="/processing-batch"
+            element={<ProcessingBatchDetails />}
+          />
+          <Route path="/tracking/:id" element={<QR sensorId={1} />} />
+          <Route path="/trackyourOrder" element={<QRTracking />} />
 
-                <Route path = "/batch-details" element={<BatchDetails/>} />
-                <Route path = "/shipping-batch" element = {<ShippingBatchDetails/>} />
-                <Route path = "/processing-batch" element = {<ProcessingBatchDetails/>} />
-                <Route path = "/tracking/:id" element = {<QR sensorId={1} />} />
-                <Route path = "/trackyourOrder" element = {<QRTracking />} />
+          {/* <Route path = "/scanqr" element = {<QRscanner />} /> */}
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
 
-                {/* <Route path = "/scanqr" element = {<QRscanner />} /> */}
-                
-            </Routes>
-        </Router>
-        </AuthProvider>
-    )
-}
-
-export default App
+export default App;
